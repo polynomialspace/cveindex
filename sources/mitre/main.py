f02f86effee840beb0a2fca1c73e08bf79194c64
polynomial space <git@polynomial.space>
2019-01-26 01:44:42 -0800

    Add mitre as a source
    The cracks are starting to show in how I originally planned to use
    this. Mitre doesn't provide easily-parsable information on *what*
    a CVE is coming from (such as a library name). I'm not sure how to
    have a less-noisy way to compare data between distro sources and
    mitre.

---
+++ sources/mitre/main.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import csv
+from sources.common import *
+
+class Main(Source):
+    def __init__(self, src="mitre", url=None, path=None, cves=None):
+        super().__init__(src, url, path, cves)
+
+    def Get(self):
+        path    = self.path
+        # This should maybe be converted to DictReader?
+        data    = csv.reader(open(path, encoding="ISO-8859-1")) # 😶 😶 😶
+        cvelist = []
+
+        for row in data:
+            if row[0].startswith("CVE"):
+                if not row[2].startswith("** RESERVED **"):
+                    # mitre doesn't provide source/package info as it doesnt
+                    # apply a lot of the time, and descriptions are too
+                    # varadic to sanely parse it.
+                    cvelist.append(CVE(row[0], "NULL", row[2]))
+
+        return cvelist
